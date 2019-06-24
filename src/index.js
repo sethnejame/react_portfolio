9b910cba495430532e8b848cffe86c5b295332dd
Seth NeJame <sethnejame@gmail.com>
2019-06-24 16:40:16 +0200

    Adding styled links About/Projects w/ corresponding pages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import Hello from "./Hello"
 import Header from "./Header"
 import Footer from "./Footer"
 import './css/tailwind.css';
+import { BrowserRouter } from 'react-router-dom'
+import Projects from "./Projects"
+import About from "./About"
+import { Switch, Route } from 'react-router-dom'
+
 
 const App = () => {
     return (
@@ -11,12 +16,20 @@ const App = () => {
             <div className="page-content">
                 <Header />
                 <div className="content-wrapper">
-                    <Hello />
+                    <Switch>
+                        <Route exact path='/' component={Hello}></Route>
+                        <Route exact path='/about' component={About}></Route>
+                        <Route exact path='/projects' component={Projects}></Route>
+                    </Switch>
                 </div>
                 <Footer />
             </div >
         </div >
-)
+    )
 };
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('app'));
