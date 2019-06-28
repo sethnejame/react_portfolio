a7ab33a74b432c8fcc8d61b494c2cee269cf0898
Seth NeJame <sethnejame@gmail.com>
2019-06-28 11:39:22 +0200

    Additional styling, removed padding, clean hover effect @ main

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,22 +3,21 @@ import React from "react"
 
 const Main = () => {
     return (
-        <div className="">
-            <div>
+        <div>
             
-                <div className="bg-teal w-full">
+                <div className="hover:bg-teal w-screen border-0 p-0 m-0">
                     <h1 className="text-5xl font-black font-sans tracking-normal">portfolio</h1>
                 </div>
 
-                <div className="bg-pink w-full">
+                <div className="hover:bg-pink w-screen">
                     <h1 className="text-5xl font-black font-sans tracking-normal">resume</h1>
                 </div>
 
-                <div className="bg-yellow w-full">
+                <div className="hover:bg-yellow w-screen">
                     <h1 className="text-5xl font-black font-sans tracking-normal">contact</h1>
                 </div>
         
-            </div>
+          
         </div>
     )
 }
