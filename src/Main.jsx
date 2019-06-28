9bb42f63ccb88400da2da6f13950a32b2940ef9f
Seth NeJame <sethnejame@gmail.com>
2019-06-28 14:09:23 +0200

    Styling main w/ gradual fade-in animation

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,21 +5,22 @@ const Main = () => {
     return (
         <div>
             
-                <div className="hover:bg-teal w-screen border-0 p-0 m-0">
-                    <h1 className="text-5xl font-black font-sans tracking-normal">portfolio</h1>
+                <div className="hover:bg-teal w-screen h-24" style={{transition: 'background-color .5s linear'}}>
+                    <h1 className="text-5xl font-black font-sans tracking-normal py-4 pl-24">portfolio</h1>
                 </div>
 
-                <div className="hover:bg-pink w-screen">
-                    <h1 className="text-5xl font-black font-sans tracking-normal">resume</h1>
+                <div className="hover:bg-pink w-screen h-24" style={{ transition: 'background-color .5s linear'}}>
+                    <h1 className="text-5xl font-black font-sans tracking-normal py-4 pl-24" >resume</h1>
                 </div>
 
-                <div className="hover:bg-yellow w-screen">
-                    <h1 className="text-5xl font-black font-sans tracking-normal">contact</h1>
+                <div className="hover:bg-yellow w-screen h-24" style={{transition: 'background-color .5s linear'}}>
+                    <h1 className="text-5xl font-black font-sans tracking-normal py-4 pl-24">contact</h1>
                 </div>
         
           
         </div>
     )
 }
+
 
 export default Main
