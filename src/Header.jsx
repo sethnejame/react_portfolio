504ed173514e7b0a2a037b34851c96b6810b38fb
Seth NeJame <sethnejame@gmail.com>
2019-07-31 08:26:29 +0200

    Adding email link to navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
                                 <li className="mr-6"><NavLink className="text-black hover:text-grey no-underline" style={{ fontFamily: 'Shrikhand', fontWeight: 'normal'}} to='/'>main</NavLink></li>
                                 <li className="mr-6"><NavLink className="text-black hover:text-teal no-underline" style={{ fontFamily: 'Shrikhand', fontWeight: 'normal'}} to='/'>portfolio</NavLink></li>
                                 <li className="mr-6"><NavLink className="text-black hover:text-pink no-underline" style={{ fontFamily: 'Shrikhand', fontWeight: 'normal'}} to='/resume'>resume</NavLink></li>
-                                <li className="mr-6"><NavLink className="text-black hover:text-yellow no-underline" style={{ fontFamily: 'Shrikhand', fontWeight: 'normal'}} to='/contact'>contact</NavLink></li>
+                                <li className="mr-6"><a className="text-black hover:text-yellow no-underline" style={{ fontFamily: 'Shrikhand', fontWeight: 'normal'}} href="mailto:sethnejame@gmail.com">contact</a></li>
                         </ul>
                         <h1 className="header-title">
                           <a target="_blank" className="text-black hover:text-grey no-underline" style={{ fontFamily: 'Shrikhand', fontWeight: 'normal'}} href='https://www.linkedin.com/in/seth-nejame-b02039183'>seth.nejame</a>
